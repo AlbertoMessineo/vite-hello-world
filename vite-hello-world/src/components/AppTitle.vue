d6c9bbac3de72e625abd8fb9381524afa28d4110
AlbertoMessineo <albevorox@gmail.com>
2023-12-11 22:21:54 +0100

    es. fixed (wasn't public before)

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script >
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<template>
+ <h1>La mia prima app con questo complicatissimo Vite!</h1>
+</template>
+
+<style>
+h1{
+  text-transform: uppercase;
+  color: black;
+  font-size: 50px;
+}
+
+</style>
